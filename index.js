e63aa4f205a981470b9e88cb9bba2f669c089ab1
Xilerth <Xilerth@gmail.com>
2016-10-17 18:56:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function cambio(e){
-  document.getElementById("imagen1").className = "":
+  document.getElementById("imagen1").className = "";
   document.getElementById("imagen1").classList.add(e.value);
 }
